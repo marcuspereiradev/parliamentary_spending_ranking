ff784091769a1137d307ed47726b40cd001596d0
Marcus Pereira <marcus.work@hotmail.com>
2021-02-26 10:47:19 -0300

    Creating csv import view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'deputies/index'
+
+  get 'csv_import/index'
+  post 'csv_import', to: 'csv_import#csv_import'
+
+  root to: 'deputies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
