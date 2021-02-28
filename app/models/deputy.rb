ebf8b9ed7d6357d63e544783f3a3da4310235357
Marcus Pereira <marcus.work@hotmail.com>
2021-02-28 18:39:00 -0300

    Adding validates to models

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -2,4 +2,6 @@ class Deputy < ApplicationRecord
   has_many :spents
 
   validates :ideCadastro, presence: true, uniqueness: true
+  validates :txNomeParlamentar, presence: true, uniqueness: true
+  validates :sgUF, presence: true
 end
--- app/models/spent.rb
+++ app/models/spent.rb
@@ -1,3 +1,5 @@
 class Spent < ApplicationRecord
   belongs_to :deputy
+
+  validates :vlrLiquido, presence: true
 end
