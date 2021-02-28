7967ed1803cb91d56a345880ca7df9a931bca5f4
Marcus Pereira <marcus.work@hotmail.com>
2021-02-28 19:11:50 -0300

    Go to top arrow

---
+++ app/javascript/custom/main.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+  var mybutton = document.getElementById("myBtn");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 300 ||
+      document.documentElement.scrollTop > 300
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  mybutton.addEventListener("click", topFunction, false);
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+});
