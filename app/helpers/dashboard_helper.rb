ca089d8fc1889d4911f464a210684a1b309887cb
Marcus Pereira <marcus.work@hotmail.com>
2021-02-28 12:22:06 -0300

    Adding format date and improving styles

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,4 +3,7 @@ module DashboardHelper
     number_to_currency(value, :unit => "R$ ", :separator => ",", :delimiter => ".")
   end
 
+  def date_to_br_date(date)
+    date.strftime("%d/%m/%Y") unless date.nil?
+  end
 end
