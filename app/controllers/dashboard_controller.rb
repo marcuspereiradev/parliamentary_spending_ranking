14815f234a702952e74a23fae314e4c8dbbc5f4e
Marcus Pereira <marcus.work@hotmail.com>
2021-03-01 15:10:26 -0300

    Adding search to find a deputy

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
   def index
-    @deputies = Deputy.all
+    if params[:q].present?
+      @deputies = Deputy.where("tx_nome_parlamentar ILIKE ?", "%#{params[:q]}%")
+      redirect_to root_path, alert: "Não foi possível encontrar o candidato solicitado" if @deputies.empty?
+    else
+      @deputies = Deputy.all
+    end
   end
 
   def show
