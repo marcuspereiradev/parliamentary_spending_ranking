3aacb3cb07f76c9009af4c5fef355b84b1f0b117
Marcus Pereira <marcus.work@hotmail.com>
2021-03-01 23:37:59 -0300

    reinforcing CSV validations

--- app/controllers/csv_import_controller.rb
+++ app/controllers/csv_import_controller.rb
@@ -6,8 +6,10 @@ class CsvImportController < ApplicationController
     if params["csv"].present? && params["csv"].tempfile.path.include?(".csv")
       CsvImporter.import(params)
       redirect_to root_path, notice: "Arquivos importados com sucesso!"
+    elsif params["csv"].present?  && !params["csv"].tempfile.path.include?(".csv")
+      redirect_to csv_import_index_path, alert: "Não é um arquivo CSV válido."
     else
-      redirect_to csv_import_index_path, alert: "Você precisa escolher um arquivo CSV!"
+      redirect_to csv_import_index_path, alert: "Você precisa escolher um arquivo CSV."
     end
   end
 end
