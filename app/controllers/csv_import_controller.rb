5c61285817f97258b8390b68042005e9d2bbd867
Marcus Pereira <marcus.work@hotmail.com>
2021-04-24 11:21:36 -0300

    Copy csv path to tmp folder before rails unlink it

--- app/controllers/csv_import_controller.rb
+++ app/controllers/csv_import_controller.rb
@@ -4,8 +4,11 @@ class CsvImportController < ApplicationController
 
   def csv_import
     if params["csv"].present? && params["csv"].tempfile.path.include?(".csv")
-      CsvImporter.import(params["csv"])
-      redirect_to root_path, notice: "Arquivos importados com sucesso!"
+      app_temp_file_path = "/tmp/#{SecureRandom.urlsafe_base64}.csv"
+      IO.copy_stream(params["csv"].tempfile.path, app_temp_file_path)
+
+      ImportCsvJob.perform_later(app_temp_file_path)
+      flash[:alert] = "Aguarde enquanto importamos os dados..."
     elsif params["csv"].present?  && !params["csv"].tempfile.path.include?(".csv")
       redirect_to csv_import_index_path, alert: "Não é um arquivo CSV válido."
     else
