8ea7dc21c1ed70bb6640ba9cd02f3f6cb6d8fb0f
Marcus Pereira <marcus.work@hotmail.com>
2021-05-30 13:59:32 -0300

    Destroy all spents belong to a Deputy

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Deputy Model
 class Deputy < ApplicationRecord
-  has_many :spents
+  has_many :spents, dependent: :destroy
 
   validates :ide_cadastro, presence: true, uniqueness: true
   validates :tx_nome_parlamentar, presence: true, uniqueness: true
