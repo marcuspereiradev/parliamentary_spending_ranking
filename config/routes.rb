5288889432671c3eee706a9dae095a2330b5a420
Marcus Pereira <marcus.work@hotmail.com>
2024-01-20 15:55:09 -0300

    feat: Adding sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  mount Sidekiq::Web, at: '/sidekiq'
 end
