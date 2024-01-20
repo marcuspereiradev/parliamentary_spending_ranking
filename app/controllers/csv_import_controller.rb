8ae80b46157a4a8f47a55d8a0d60463d00f8eb0a
Marcus Pereira <marcus.work@hotmail.com>
2024-01-20 15:47:51 -0300

    Refactorying csv importer controller

--- app/controllers/csv_import_controller.rb
+++ app/controllers/csv_import_controller.rb
@@ -1,18 +1,32 @@
+# frozen_string_literal: true
+
+# CsvImportController
 class CsvImportController < ApplicationController
   def index; end
 
   def csv_import
-    if params['csv'].present? && params['csv'].tempfile.path.include?('.csv')
-      app_temp_file_path = "/tmp/#{SecureRandom.urlsafe_base64}.csv"
-      IO.copy_stream(params['csv'].tempfile.path, app_temp_file_path)
+    return redirect_to csv_import_index_path, alert: 'Não é um arquivo CSV válido.' unless csv_valid?
 
-      ImportCsvJob.perform_later(app_temp_file_path)
+    if csv_valid?
+      app_temp_file_path = build_csv_file_path
+
+      ImportCsvJob.perform_now(app_temp_file_path)
+
       redirect_to root_path
-      flash[:alert] = 'Aguarde enquanto importamos os dados...'
-    elsif params['csv'].present? && !params['csv'].tempfile.path.include?('.csv')
-      redirect_to csv_import_index_path, alert: 'Não é um arquivo CSV válido.'
-    else
-      redirect_to csv_import_index_path, alert: 'Você precisa escolher um arquivo CSV.'
+      return flash[:alert] = 'Aguarde enquanto importamos os dados...'
     end
+
+    redirect_to csv_import_index_path, alert: 'Você precisa escolher um arquivo CSV.'
+  end
+
+  def build_csv_file_path
+    app_temp_file_path = "/tmp/#{SecureRandom.urlsafe_base64}.csv"
+    IO.copy_stream(params['csv'].tempfile.path, app_temp_file_path)
+
+    app_temp_file_path
+  end
+
+  def csv_valid?
+    params['csv'].present? && params['csv'].tempfile.path.include?('.csv')
   end
 end
