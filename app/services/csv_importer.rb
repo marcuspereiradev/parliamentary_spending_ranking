d8b4036faae5a8a5e51ee4ab2993a4d9bc2e93e6
Marcus Pereira <marcus.work@hotmail.com>
2024-01-20 15:46:24 -0300

    Refactorying csv importer service

--- app/services/csv_importer.rb
+++ app/services/csv_importer.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'csv'
 
 # Import CSV
@@ -7,58 +8,48 @@ class CsvImporter
     cleaned_csv = File.read(file).delete('"')
     csv = CSV.parse(cleaned_csv, headers: true, header_converters: ->(f) { f.gsub(/[^0-9A-Za-z]/, '') }, col_sep: ';')
 
-    csv.each do |row|
-      next unless row[5] == 'RJ'
-
-      parliamentary = parliamentarians_fields
-      spents = spents_fields
-
-      parliamentary[:tx_nome_parlamentar] = row[0]
-      parliamentary[:ide_cadastro] = row[2]
-      parliamentary[:sg_uf] = row[5]
-      parliamentary[:sg_partido] = row[6]
-      parliamentary[:avatar] = "https://www.camara.leg.br/internet/deputado/bandep/#{row[2]}.jpgmaior.jpg"
-      parliamentary[:avatar_congresso] = "https://www.camara.leg.br/internet/deputado/bandep/pagina_do_deputado/#{row[2]}.jpg"
-
-      spents[:dat_emissao] = row[10]
-      spents[:txt_fornecedor] = row[12]
-      spents[:vlr_liquido] = row[19]
-      spents[:url_documento] = row[30]
-
-      parliamentary_saved = create_deputy(parliamentary)
-      create_spents(parliamentary_saved, spents)
-    end
+    save_csv_data(csv)
   ensure
     File.delete(file)
   end
 
   class << self
-    def parliamentarians_fields
+    def save_csv_data(csv)
+      csv.each do |row|
+        next unless row[5] == 'RJ'
+
+        parliamentary = build_parliamentary(row)
+        parliamentary_saved = create_parliamentary(parliamentary)
+
+        spents = build_spent(row)
+        parliamentary_saved.spents.create(spents)
+
+        parliamentary_saved.update(total_spents: parliamentary_saved.spents.pluck(:vlr_liquido).sum)
+      end
+    end
+
+    def build_parliamentary(row)
       {
-        tx_nome_parlamentar: '',
-        ide_cadastro: '',
-        sg_uf: '',
-        sg_partido: '',
-        avatar: '',
-        avatar_congresso: ''
+        tx_nome_parlamentar: row[0],
+        ide_cadastro: row[2],
+        sg_uf: row[5],
+        sg_partido: row[6],
+        avatar: "https://www.camara.leg.br/internet/deputado/bandep/#{row[2]}.jpgmaior.jpg",
+        avatar_congresso: "https://www.camara.leg.br/internet/deputado/bandep/pagina_do_deputado/#{row[2]}.jpg"
       }
     end
 
-    def spents_fields
+    def build_spent(row)
       {
-        vlr_liquido: '',
-        txt_fornecedor: '',
-        url_documento: '',
-        dat_emissao: ''
+        dat_emissao: row[10],
+        txt_fornecedor: row[12],
+        vlr_liquido: row[19],
+        url_documento: row[30]
       }
     end
 
-    def create_deputy(parliamentary)
+    def create_parliamentary(parliamentary)
       Parliamentary.find_or_create_by(parliamentary)
-    end
-
-    def create_spents(parliamentary_saved, spents)
-      parliamentary_saved.spents.create(spents)
     end
   end
 end
